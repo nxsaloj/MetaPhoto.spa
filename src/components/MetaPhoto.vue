54d5d96b2a9c0e6cbbb4cb5574767dfc41085aba
nxsaloj <niisaloj@gmail.com>
2024-08-13 17:01:24 -0600

    Initializing MetaPhoto components and configurations

---
+++ src/components/MetaPhoto.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <section>{{ title }}</section>
+  </div>
+</template>
+<script setup lang="ts">
+const props = defineProps({
+  id: Number,
+  title: String,
+});
+</script>
---
+++ src/views/PhotoView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>Photo view</h1>
+    <MetaPhoto
+      v-for="(photo, index) in photos"
+      :key="index"
+      :title="photo.title"
+    ></MetaPhoto
+    ><br />
+  </div>
+</template>
+<script setup>
+import { ref, onBeforeMount } from "vue";
+import MetaPhoto from "@/components/MetaPhoto.vue";
+
+const photos = ref([]);
+
+onBeforeMount(() => {
+  for (let i = 1; i < 20; i++) {
+    photos.value.push({ id: i, title: "Title" + i });
+  }
+});
+</script>
