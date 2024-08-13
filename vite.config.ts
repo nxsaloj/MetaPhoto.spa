311ac8dc5319a3ce38314daac45523c59d419310
nxsaloj <niisaloj@gmail.com>
2024-08-12 21:43:21 -0600

    Adding vue plugin in vite and aliasing source dir

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,14 @@
 import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { resolve } from "path";
 
 export default defineConfig({
-  plugins: [],
+  plugins: [vue()],
+  base: "./",
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
+    },
+  },
+  define: { "process.env": {} },
 });
